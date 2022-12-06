ce24a94e7ed9df188e7e5c28f0557a1a25ba7ece
Nandini Mundra <nandinimundra00@gmail.com>
2022-12-05 19:08:45 -0500

    new functionality to videdeadlines.py

--- commands/viewdeadlinetasks.py
+++ commands/viewdeadlinetasks.py
@@ -15,3 +15,34 @@ class ViewDeadlineTasks:
 
         """
         self.payload = {"response_type": "ephemeral", "blocks": []}
+    
+    def get_list(self):
+        """
+        Return a list of tasks formatted in a slack message payload.
+
+        :param None:
+        :type None:
+        :raise None:
+        :return: Slack message payload with list of tasks.
+        :rtype: dict
+
+        """
+        tasks = []
+        # db query to get all tasks that have progress < 1
+        
+        tasks_with_deadline = (
+            Task.query.join(Assignment)
+            .add_columns(
+                Assignment.user_id,
+                Assignment.progress,
+                Task.task_id,
+                Task.points,
+                Task.description,
+                Task.deadline,
+            )
+            .filter(Task.deadline == datetime.utcnow().strftime("%Y-%m-%d"))
+            .filter(Assignment.progress < 1)
+            .all()
+        )
+        tasks.extend(tasks_with_deadline)
+
