61cb7a3b2e1c6c5dc155477e79ac6de57d3b9b38
Kii4ka <evilkomir@gmail.com>
2024-10-29 09:05:00 -0400

    Updated functionality issue for interactive functionality (#11)

--- app.py
+++ app.py
@@ -54,11 +54,73 @@ def findName(slack_id, channel_id):
 
 @app.route("/slack/interactive-endpoint", methods=["POST"])
 def interactive_endpoint():
+    """
+     All interactive events like button click, input fields are received in this endpoint. We use this endpoint to handle the click event of 'Add task' button of create-task command.
+     :param:
+     :type:
+     :raise:
+     :return: Response object with 200 HTTP status
+     :rtype: Response
+     """
     payload = json.loads(request.form.get("payload"))
     if payload["type"] == "block_actions":
         actions = payload["actions"]
         if len(actions) > 0:
-            if actions[0]["action_id"] == "submit_reminder":
+            if actions[0]["action_id"] in ["create_action_button", "update_action_button"]:
+                # Create Task or Update Task - button was clicked
+                channel_id = payload["container"]["channel_id"]
+                user_id = payload["user"]["id"]
+                helper = ErrorHelper()
+                ct = CreateTask()
+                ut = UpdateTask(user_id=user_id)
+                state_values = payload["state"]["values"]
+                desc = None
+                deadline = None
+                points = None
+                assignee = None
+                for _, val in state_values.items():
+                    if "create_action_description" in val:
+                        desc = val["create_action_description"]["value"]
+                    elif "create_action_deadline" in val:
+                        deadline = val["create_action_deadline"]["selected_date"]
+                    elif "create_action_points" in val:
+                        if val["create_action_points"]["selected_option"] is not None:
+                            points = val["create_action_points"]["selected_option"][
+                                "value"
+                            ]
+                        else:
+                            points = None
+                    elif "create_action_assignees" in val:
+                        if val["create_action_assignees"]["selected_option"] is not None:
+                            assignee = val["create_action_assignees"]["selected_option"]["value"]
+                if desc is None or deadline is None or points is None:
+                    error_blocks = helper.get_error_payload_blocks("createtask")
+                    slack_client.chat_postEphemeral(
+                        channel=channel_id, user=user_id, blocks=error_blocks
+                    )
+                else:
+                    id = None
+                    if (actions[0]["action_id"] == "create_action_button"):
+                        blocks, id = ct.create_task(desc=desc, points=points, deadline=deadline, assignee=assignee,
+                                                    created_by=user_id)
+                        slack_client.chat_postEphemeral(
+                            channel=channel_id, user=user_id, blocks=blocks
+                        )
+                    else:
+                        id = payload["actions"][0]["value"]
+                        blocks = ut.update_task(id=id, desc=desc, points=points, deadline=deadline, assignee=assignee)
+                        slack_client.chat_postEphemeral(
+                            channel=channel_id, user=user_id, blocks=blocks
+                        )
+                    if (assignee):
+                        assignerName = findName(user_id, channel_id)
+
+                        message = "Task #" + str(id) + " has been assigned to you by " + assignerName
+                        slack_client.chat_postEphemeral(
+                            channel=channel_id, user=assignee,
+                            blocks=[{"type": "section", "text": {"type": "plain_text", "text": message}}]
+                        )
+            elif actions[0]["action_id"] == "submit_reminder":
                 selected_date = payload["state"]["values"]["reminder_date"]["select_date"]["selected_date"]
                 selected_time = payload["state"]["values"]["reminder_time"]["select_time"]["selected_time"]
                 reminder_message = payload["state"]["values"]["reminder_message"]["message_input"]["value"]
