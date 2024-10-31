9c5cc86aa5da0f91fb5c58c4ea50131be3ddc987
Kii4ka <evilkomir@gmail.com>
2024-10-30 21:46:28 -0400

    Added test for empty channel name (#5)

--- tests/test_reminders.py
+++ tests/test_reminders.py
@@ -12,6 +12,13 @@ class TestAppFunctions(unittest.TestCase):
         channel_id = get_channel_id('general!')
         self.assertIsNone(channel_id)
 
+    @patch('app.slack_client.conversations_list')
+    def test_get_channel_id_with_empty_string(self, mock_conversations_list):
+        """Test get_channel_id with an empty string as the channel name."""
+        mock_conversations_list.return_value = {'channels': [{'name': 'general', 'id': 'C123456'}]}
+        channel_id = get_channel_id('')
+        self.assertIsNone(channel_id)
+
 
 if __name__ == '__main__':
     unittest.main()
