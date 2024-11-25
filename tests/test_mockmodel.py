0790208da7387e1552183f1b82db576db6394438
karthikeya7746 <karthikeya.7746@gmail.com>
2024-11-24 19:55:32 -0500

    Added testcases to test_mockmodel.py

--- tests/test_mockmodel.py
+++ tests/test_mockmodel.py
@@ -35,3 +35,37 @@ def test_sqlalchemy_query_property_get_mock(
     )
 
     assert response == [mock_my_model]
+    
+def test_sqlalchemy_query_for_completed_tasks(
+    mock_my_model,
+    mock_get_sqlalchemy,
+):
+    """
+    Test query to fetch tasks where the progress is marked as 1 (completed)
+
+    :param mock_my_model: Mocked Task object
+    :type mock_my_model: Task
+    :param mock_get_sqlalchemy: Mocked SQLAlchemy object
+    :type mock_get_sqlalchemy: Any
+    :raise:
+    :return: Assert if test case executed successfully
+    :rtype: bool
+    """
+    mock_get_sqlalchemy.join.return_value.add_columns.return_value.filter.return_value.all.return_value = [
+        mock_my_model
+    ]
+    response = (
+        Task.query.join(Assignment)
+        .add_columns(
+            Assignment.progress,
+            Task.task_id,
+            Task.points,
+            Task.description,
+            Task.deadline,
+        )
+        .filter(Assignment.progress == 1.0)  # Completed tasks
+        .all()
+    )
+
+    assert response == [mock_my_model]
+
