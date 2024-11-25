6d8d9531641cba2f2189ff763e49dc8fd5fbac84
karthikeya7746 <karthikeya.7746@gmail.com>
2024-11-24 19:29:40 -0500

    Added a test case in test_help.py file

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,23 +1,12 @@
 from commands.help import Help
 
-
 def test_help():
     """
     Test the help command
-
-    :param:
-    :type:
-    :raise:
-    :return: Assert if test case executed successfully
-    :rtype: bool
-
     """
-
-    # test function
     h = Help()
     payload = h.help_all()
 
-    # expectation
     expected_payload = {
         "response_type": "ephemeral",
         "blocks": [
@@ -29,10 +18,7 @@ def test_help():
                     "text": ">To create a task, just try the command */create-task* and you would receive a message from Slack to fill out the details of the task.\n>Enter the description, deadline and the points of the task.\n>For example:\n>*Description*: Hey! This is my new task\n>*Deadline*: 12/31/2022 (just select a date from the date picker)\n>*Points*: 5 (select a point from 1 to 5)\n>And that's it! You should receive a reply from Slack with the generated *Task ID*.",
                 },
             },
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*View Completed Tasks*"},
-            },
+            {"type": "section", "text": {"type": "mrkdwn", "text": "*View Completed Tasks*"}},
             {
                 "type": "section",
                 "text": {
@@ -40,10 +26,7 @@ def test_help():
                     "text": ">To view completed tasks, just try the command */view-completed*, and there you go! SlackPoint would show you a list of completed tasks.",
                 },
             },
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*View Pending Task*"},
-            },
+            {"type": "section", "text": {"type": "mrkdwn", "text": "*View Pending Task*"}},
             {
                 "type": "section",
                 "text": {
@@ -51,15 +34,12 @@ def test_help():
                     "text": ">To view pending tasks, just try the command */view-pending*, and there you go! SlackPoint would show you a list of completed tasks. To view pending tasks only for yourself, just try the command */view-pending me*, and there you go! SlackPoint would show you a list of tasks assigned to you. To view pending tasks due today, just try the command */view-pending today*, and SlackPoint will show you a list of tasks with today's deadline.",
                 },
             },
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*Update Task*"},
-            },
+            {"type": "section", "text": {"type": "mrkdwn", "text": "*Update Task*"}},
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text":  ">To update a task, just run the command */update-task* <Task ID>, edit the details and hit submit!",
+                    "text": ">To update a task, just run the command */update-task* <Task ID>, edit the details and hit submit!",
                 },
             },
             {"type": "section", "text": {"type": "mrkdwn", "text": "*Leaderboard*"}},
@@ -89,7 +69,15 @@ def test_help():
         ],
     }
 
-    print(payload)
-    print("*************")
-    print(expected_payload)
     assert payload == expected_payload
+
+
+def test_empty_help():
+    h = Help()
+    h.commands_dictionary = {}
+    payload = h.help_all()
+    expected_payload = {"response_type": "ephemeral", "blocks": []}
+    assert payload == expected_payload
+
+
+
