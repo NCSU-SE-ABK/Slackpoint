81061aa12577ba42cac1cda4bdff3f25a732c379
bittubharadwaj <bittukatna@gmail.com>
2024-11-24 20:04:25 -0500

    no updations in test_help.py

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,4 +1,8 @@
-from commands.help import Help
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from commands.help import Help # type: ignore
+
 
 
 def test_help():
