135027de80d4c738fecf98fda4e5e29aa0e76f0b
bittubharadwaj <bittukatna@gmail.com>
2024-11-25 19:10:26 -0500

    added three new test cases

--- tests/test_viewcompleted.py
+++ tests/test_viewcompleted.py
@@ -158,6 +158,36 @@ def test_view_completed_1task_different_task(mock_completed_task_4, mock_get_sql
     assert payload == expected_payload
 
 
+def test_view_completed_1task_different_task1(mock_completed_task_4, mock_get_sqlalchemy):
+    """
+    Test the view completed command with one task (different from the original).
+    """
+    # Mocking DB call
+    mock_get_sqlalchemy.join.return_value.add_columns.return_value.filter.return_value.all.return_value = [
+        mock_completed_task_4,
+    ]
+
+    # Test function
+    vp = ViewPoints(progress=1.0)
+    payload = vp.get_list()
+
+    # Expected output
+    expected_payload = {
+        "response_type": "ephemeral",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ">SP-4 (5 SlackPoints) This is Task 4 [Deadline: 2022-08-26]",
+                },
+            }
+        ],
+    }
+
+    assert payload == expected_payload
+
+
 def test_view_completed_2tasks_same_deadlines(
     mock_completed_task_3,
     mock_completed_task_4,
@@ -203,6 +233,52 @@ def test_view_completed_2tasks_same_deadlines(
     assert payload == expected_payload
 
 
+def test_view_completed_2tasks_same_deadlines1(
+    mock_completed_task_3,
+    mock_completed_task_4,
+    mock_get_sqlalchemy,
+):
+    """
+    Test the view completed command with two tasks having the same deadline.
+    """
+    # Mocking DB call
+    mock_completed_task_3.deadline = "2022-08-27"
+    mock_completed_task_4.deadline = "2022-08-28"
+
+    mock_get_sqlalchemy.join.return_value.add_columns.return_value.filter.return_value.all.return_value = [
+        mock_completed_task_3,
+        mock_completed_task_4,
+    ]
+
+    # Test function
+    vp = ViewPoints(progress=1.0)
+    payload = vp.get_list()
+
+    # Expectation
+    expected_payload = {
+        "response_type": "ephemeral",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ">SP-3 (5 SlackPoints) This is Task 3 [Deadline: 2022-08-27]",
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ">SP-4 (5 SlackPoints) This is Task 4 [Deadline: 2022-08-28]",
+                },
+            },
+        ],
+    }
+
+    assert payload == expected_payload
+
+
+
 def test_view_completed_0tasks_varying_message(mock_get_sqlalchemy):
     """
     Test the view completed command with no tasks and a custom empty message.
@@ -230,3 +306,29 @@ def test_view_completed_0tasks_varying_message(mock_get_sqlalchemy):
 
     assert payload == expected_payload
 
+def test_view_completed_0tasks_varying_message1(mock_get_sqlalchemy):
+    """
+    Test the view completed command with no tasks and a custom empty message.
+    """
+    # Mocking DB call
+    mock_get_sqlalchemy.join.return_value.add_columns.return_value.filter.return_value.all.return_value = []
+
+    # Test function
+    vp = ViewPoints(progress=1.0)
+    payload = vp.get_list()
+
+    # Expectation
+    expected_payload = {
+        "response_type": "ephemeral",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ">Currently there are no SlackPoints available",
+                },
+            }
+        ],
+    }
+
+    assert payload == expected_payload
